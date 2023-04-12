b5eeeb30c5a1995b2864b6e3ffc8a906121c0a05
Achtuur <arthurdegroot88@gmail.com>
2023-04-12 10:18:09 +0200

    add some vram abstraction

--- src/ppu/memory/vram.rs
+++ src/ppu/memory/vram.rs
@@ -1,6 +1,38 @@
 const VRAM_SIZE: usize = 0x8000;
+
+/// Increment mode for VRAM based on byte written to `$2115`
+enum IncrementMode {
+    /// Increment when reading/writing to low VRAM byte
+    /// 
+    /// `$2118` for write and `$2139` for read respectively
+    LowByte, 
+
+    /// Increment when reading/writing to high VRAM byte
+    /// 
+    /// `$2119` for write and `$213A` for read respectively
+    HighByte
+}
+
+enum AddrRemap {
+    /// No remapping
+    NoRemap,
+    /// Remap `rrrrrrrr YYYccccc -> rrrrrrrr cccccYYY` (2bpp)
+    TwoBpp,
+    /// Remap `rrrrrrrY YYcccccP -> rrrrrrrc ccccPYYY` (4bpp)
+    FourBpp,
+    /// Remap `rrrrrrYY YcccccPP -> rrrrrrcc cccPPYYY` (8bpp)
+    EighBpp
+}
+
 pub struct Vram {
     bytes: [u16; VRAM_SIZE],
+
+    // Vmain values ($2115)
+    incr_mode: IncrementMode,
+    incr_amount: usize,
+    addr_remap: AddrRemap,
+
+
     vmain: u8, // $2115
     vmaddl: u8, // $2116
     vmaddh: u8, // $2117
@@ -35,7 +67,7 @@ impl Vram {
 
     pub fn write(&mut self, addr: u16, byte: u8) {
         match addr {
-            0x2115 => ,
+            0x2115 => self.write_vmain(byte),
             0x2116 => ,
             0x2117 => ,
             0x2118 => ,
@@ -43,5 +75,31 @@ impl Vram {
 
             _ => {},
         }
+    }
+
+    /// 7  bit  0
+    /// ---- ----
+    /// M... RRII
+    /// |    ||||
+    /// |    ||++- Address increment amount:
+    /// |    ||     0: Increment by 1 word
+    /// |    ||     1: Increment by 32 words
+    /// |    ||     2: Increment by 128 words
+    /// |    ||     3: Increment by 128 words
+    /// |    ++--- Address remapping: (VMADD -> Internal)
+    /// |           0: None
+    /// |           1: Remap rrrrrrrr YYYccccc -> rrrrrrrr cccccYYY (2bpp)
+    /// |           2: Remap rrrrrrrY YYcccccP -> rrrrrrrc ccccPYYY (4bpp)
+    /// |           3: Remap rrrrrrYY YcccccPP -> rrrrrrcc cccPPYYY (8bpp)
+    /// +--------- Address increment mode:
+    /// 0: Increment after writing $2118 or reading $2139
+    /// 1: Increment after writing $2119 or reading $213A
+    fn write_vmain(&mut self, byte: u8) {
+        self.incr_amount = match (byte & 0x1, (byte >> 1) & 0x1) {
+            (0, 0) => 1,
+            (0, 1) => 32,
+            (1, _) => 128,
+        }
+
     }
 }
